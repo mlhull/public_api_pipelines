b22853e0ea09e8b8f7d16af0a32a716397053c51
mlhull <142443797+mlhull@users.noreply.github.com>
2024-10-16 14:16:12 -0600

    Update spotify_tracks_dag.py
    DAG now uses config file and includes s3 staging bucket. Breaks out steps to achieve better atomicity between tasks

--- spotify_tracks/spotify_tracks_dag.py
+++ spotify_tracks/spotify_tracks_dag.py
@@ -3,12 +3,36 @@ from airflow.decorators import dag
 from airflow.providers.http.operators.http import SimpleHttpOperator  
 from datetime import datetime
 from airflow.operators.python import PythonOperator
-from include.spotify_etl_step import *
-import pyarrow
+from airflow.models import Variable #to locate config file
+import configparser #to read in config file
+from airflow.hooks.base import BaseHook
 
-#set api query params
-artist_id = '4Z8W4fKeB5YxbusRsdQVPb'
-params = {'market': 'US'}
+#grab etl python callable + lib to export to parquet
+from include.api_etl_module import get_client_secret_id, api_call_with_token, s_transform_and_export
+
+#get aws connections entered into Airflow UI
+aws_conn = BaseHook.get_connection('AWSConnection')
+aws_key = aws_conn.login
+aws_secret = aws_conn.password
+
+#grab config file
+config = configparser.ConfigParser()
+config.read(Variable.get('config'))
+
+#api query params + connection
+http_conn_id = config.get('spotify_api', 'http_conn_id')
+post_endpoint = config.get('spotify_api', 'post_endpoint') #this is the endpoint for the url featured in the Airflow connection
+spotify_api_endpoint = config.get('spotify_api', 'spotify_api_endpoint')#this is the endpoint to do the GET api call
+api_query=config.get('spotify_api', 'api_query')
+
+#for file name
+file_name = config.get('spotify_api','file_name')
+today_date = datetime.utcnow() #grab today's date
+date_only = today_date.date() #this will be used for the export dataset
+
+#define s3 buckets
+s3_stage = config.get('s3_buckets', 'api_stage_tbls')
+s3_results = config.get('s3_buckets', 'api_results_tbls')
 
 ##set dag
 @dag(
@@ -24,51 +48,63 @@ params = {'market': 'US'}
 
 def spotify_tracks_pipeline():
 
-    #grab airflow connection details
+    #grab airflow http connection details and pass credential using xcom
     get_connection = PythonOperator(
-        task_id='get_connection',
-        python_callable=get_spotify_credentials,
+        task_id='get_client_secret_id',
+        python_callable=get_client_secret_id,
+        op_kwargs={'http_conn_id': http_conn_id},
         do_xcom_push=True
     )
 
-    #use airflow connection to get token
+    #use airflow connection to get token and pass credential using xcom
     get_token_task = SimpleHttpOperator(
         task_id='get_token',
         method='POST',
         log_response=True,
-        http_conn_id='spotify_auth', #this gets the base url
-        endpoint='/api/token', #applied on top of base url
+        http_conn_id=http_conn_id, #this gets the base url
+        endpoint=post_endpoint, #applied on top of base url
         data=(
         'grant_type=client_credentials&'
-        'client_id={{ ti.xcom_pull(task_ids="get_connection", key="client_id") }}&'
-        'client_secret={{ ti.xcom_pull(task_ids="get_connection", key="client_secret") }}'
-    ),  #set as a data string to meet expected format
+        'client_id={{ ti.xcom_pull(task_ids="get_client_secret_id", key="client_id") }}&'
+        'client_secret={{ ti.xcom_pull(task_ids="get_client_secret_id", key="client_secret") }}'
+        ),  #set as a data string to meet expected format
         headers={'Content-Type': 'application/x-www-form-urlencoded'}, #designate expected format
         response_filter=lambda response: response.json().get('access_token'), #parses response to pass along to next task
         do_xcom_push=True
     )
 
-    #call api using token
-    get_tracks_task = SimpleHttpOperator(
-        task_id='get_tracks',
-        method='GET',
-        log_response=True,
-        http_conn_id='spotify_api',
-        endpoint= f"/v1/artists/{artist_id}/top-tracks",
-        params=params,
-        headers={"Authorization": "Bearer {{ ti.xcom_pull(task_ids='get_token', key='return_value') }}"}, #grab auth token
-        response_filter=lambda response: response.json(), #parse response as json to pass along to next task
-        do_xcom_push=True
+    #makes call using xcom token and loads json data into staging s3 bucket
+    call_to_stage = PythonOperator(
+        task_id='call_to_stage',
+        python_callable=api_call_with_token,
+        op_kwargs={
+            'url': f'{spotify_api_endpoint}{api_query}',
+            's3_stage': s3_stage,
+            'date_only': str(date_only),
+            'aws_key': aws_key,
+            'aws_secret': aws_secret,
+            'file_name': file_name,
+            'params': None,
+            'auth_token_header': '{{ task_instance.xcom_pull(task_ids="get_token") }}'  #grab auth token from previous task using xcom
+        }
     )
-    
-    #select data of interest and transform data to pd df
+
+    #grabs data from staging bucket, transforms it and loads it to results bucket
     transform_and_export_task = PythonOperator(
-        task_id='transform_and_export_tracks_data',
-        python_callable=transform_and_export,
-        provide_context=True #add context to pass ti (taskinstance)
+        task_id='process_and_load',
+        python_callable=s_transform_and_export,
+        op_kwargs={
+            's3_stage': s3_stage,
+            'date_only': str(date_only),
+            's3_results': s3_results,
+            'aws_key': aws_key,
+            'aws_secret': aws_secret,
+            'file_name': file_name
+        }
     )
 
     # Update the task dependencies
-    get_connection >> get_token_task >> get_tracks_task >> transform_and_export_task
+    get_connection >> get_token_task >> call_to_stage >> transform_and_export_task
 
 spotify_tracks_pipeline()
+
